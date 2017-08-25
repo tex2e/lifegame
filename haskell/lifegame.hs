1e61eb9a345712da9b294be4b297447d064abe78
Mako <mnfeconicu41@gmail.com>
2017-08-25 22:57:12 +0900

    haskell: update

--- haskell/lifegame.hs
+++ haskell/lifegame.hs
@@ -1,17 +1,18 @@
 
-import System.Random
+import System.Random (StdGen, getStdGen, randomRs)
 import System.Posix.Unistd (usleep)
-import Data.Array
+import Data.Array (Array, array, (!), bounds, elems)
+import Data.List (intercalate)
 import Data.List.Split (chunksOf)
 
 type Field = Array (Int, Int) Int
 
 main :: IO ()
 main = do
-  g <- getStdGen -- Gets the global random number generator.
+  gen <- getStdGen -- Gets the global random number generator.
   let height = 20
       width = 40
-  lifegame $ initField height width g
+  lifegame $ initField height width gen
 
 lifegame :: Field -> IO ()
 lifegame field = do
@@ -20,17 +21,17 @@ lifegame field = do
   usleep 100000
   lifegame $ evolve field
 
-initField :: RandomGen g => Int -> Int -> g -> Field
-initField height width g = field
-  where randomListElem = take (width * height) $ randomRs (0::Int, 1::Int) g
+initField :: Int -> Int -> StdGen -> Field
+initField height width gen = field
+  where randomListElem = take (width * height) $ randomRs (0,1) gen :: [Int]
         randomListIndices = [(y,x) | y<-[0..height-1], x<-[0..width-1]]
         randomList = zip randomListIndices randomListElem
         field = array ((0,0),(height-1, width-1)) randomList
 
 evolve :: Field -> Field
 evolve field = newField
-  where width  = (snd $ snd $ bounds field) + 1
-        height = (fst $ snd $ bounds field) + 1
+  where width  = (snd . snd . bounds) field + 1
+        height = (fst . snd . bounds) field + 1
         deadOrAlive field (y,x)
           | aliveNeighbours <= 1 = 0
           | aliveNeighbours == 2 = field ! (y,x)
@@ -42,16 +43,15 @@ evolve field = newField
 
 countAliveNeighbours :: Field -> (Int, Int) -> Int
 countAliveNeighbours field (y,x) = sum neighbours
-  where width  = (snd $ snd $ bounds field) + 1
-        height = (fst $ snd $ bounds field) + 1
+  where width  = (snd . snd . bounds) field + 1
+        height = (fst . snd . bounds) field + 1
         neighbours = [field ! (mod (y + yi) height, mod (x + xi) width) | yi<-[-1..1], xi<-[-1..1], not (xi == 0 && yi == 0)]
 
 dumpField :: Field -> IO ()
-dumpField field = putStrLn $ foldl1 join eachLines
-  where width  = (snd $ snd $ bounds field) + 1
+dumpField field = putStrLn $ intercalate "\n" eachLines
+  where width  = (snd . snd . bounds) field + 1
         toChar ch = if ch == 0 then ' ' else 'o'
-        join a b = a ++ "\n" ++ b
-        eachLines = chunksOf width $ map toChar $ elems field
+        eachLines = chunksOf width . map toChar . elems $ field
 
 clearScreen :: IO ()
 clearScreen = putStrLn "\ESC[;H\ESC[2J"

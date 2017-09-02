3df6b078ed914070b5d3a7a1e58954b6a40b43c4
Mako <mnfeconicu41@gmail.com>
2017-09-02 22:07:34 +0900

    f#: refactoring

--- f#/lifegame.fs
+++ f#/lifegame.fs
@@ -5,22 +5,20 @@ open System.Threading
 type Lifegame(height: int, width: int) =
     let randomGen = System.Random()
     let mutable field = Array2D.init height width (fun x y -> randomGen.Next(2))
-    member this.Field() = field
     member this.Loop() =
         while true do
             this.ClearScreen()
             this.DumpField()
             Thread.Sleep(100); // 100[ms]
-            field <- this.Evolve(this.Field)
+            field <- this.Evolve(field)
     member this.Evolve(field: int[,]) =
         Array2D.mapi (fun y x cell -> this.DeadOrAlive(field, y, x)) field
     member this.DeadOrAlive(field: int[,], y: int, x: int): int =
         let aliveNeighbours = this.CountAliveNeighbours(field, y, x)
         match aliveNeighbours with
-        | 0 | 1 -> 0
-        | 2     -> field.[y, x]
-        | 3     -> 1
-        | _     -> 0
+        | 2 -> field.[y, x]
+        | 3 -> 1
+        | _ -> 0
     member this.CountAliveNeighbours(field: int[,], y: int, x: int): int =
         [| for yi in -1..1 do
             for xi in -1..1 ->
@@ -35,7 +33,7 @@ type Lifegame(height: int, width: int) =
         | _ when a < 0 -> (a + b) % b
         | _ -> 0
     member this.DumpField() =
-        let charField = Array2D.map (fun cell -> if cell = 0 then ' ' else 'o') this.Field
+        let charField = Array2D.map (fun cell -> if cell = 0 then ' ' else 'o') field
         Array2D.iteri (fun y x cell ->
             if x = width - 1 then printfn "%c" cell else printf "%c" cell) charField
     member this.ClearScreen() = Console.Clear()

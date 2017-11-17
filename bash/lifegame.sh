d66cac65f5d1307ceca7682ef94998bfa959c3a3
Mako <mnfeconicu41@gmail.com>
2017-11-17 13:33:06 +0900

    bash: add vercomp to compare bash versions

--- bash/lifegame.sh
+++ bash/lifegame.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
-if [[ ! "$BASH_VERSION" =~ ^4\. ]]; then
-  echo "Error: Require bash 4.x"
+
+vercomp () {
+  if [[ $1 == $2 ]]; then
+    return 0
+  fi
+  local IFS=.
+  local i ver1=($1) ver2=($2)
+  # fill empty fields in ver1 with zeros
+  for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
+    ver1[i]=0
+  done
+  for ((i=0; i<${#ver1[@]}; i++)); do
+    if [[ -z ${ver2[i]} ]]; then
+      # fill empty fields in ver2 with zeros
+      ver2[i]=0
+    fi
+    if ((10#${ver1[i]} > 10#${ver2[i]})); then
+      return 1
+    fi
+    if ((10#${ver1[i]} < 10#${ver2[i]})); then
+      return -1
+    fi
+  done
+  return 0
+}
+
+vercomp "$BASH_VERSION" 4.0
+if [[ $? -lt 0 ]]; then
+  echo "Error: Require bash >= 4.0"
   exit
 fi
 
